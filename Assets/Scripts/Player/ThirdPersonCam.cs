e50db1e3a7a408b0e5d6603f9f6b337248cf3af9
KRIMNOUILLE <64409680+KrimKujo@users.noreply.github.com>
2022-09-25 20:32:27 +0200

    commit

--- Assets/Scripts/Player/ThirdPersonCam.cs
+++ Assets/Scripts/Player/ThirdPersonCam.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ThirdPersonCam : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header("References")]
+
+    public PlayerInput playerInput;
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+    public Rigidbody rb;
+
+    public float rotationSpeed;
+
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void Update() {
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+        float horizontalInput =  playerInput.actions.FindActionMap("Foot")["Movement"].ReadValue<Vector2>().x;
+        float verticalInput =  playerInput.actions.FindActionMap("Foot")["Movement"].ReadValue<Vector2>().y;
+        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
         
+        if(inputDir != Vector3.zero) {
+            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+        }
     }
+
 }
