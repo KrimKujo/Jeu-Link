5707d6704c1aefaf49bad31cc774fc097e58d3cd
KRIMNOUILLE <64409680+KrimKujo@users.noreply.github.com>
2022-09-27 20:39:33 +0200

    Ajout du systeme de mouvement pas terminé

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,8 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    [Header("Input")]
+    public PlayerInput playerInput;
+
+    public float groundDrag;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
+
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = playerInput.actions.FindActionMap("Foot")["Movement"].ReadValue<Vector2>().x;
+        verticalInput = playerInput.actions.FindActionMap("Foot")["Movement"].ReadValue<Vector2>().y;
+    }
+
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+
 }
